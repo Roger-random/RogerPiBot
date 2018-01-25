57255576a2bb70324883d99d7f903aeb56545778
Roger Cheng <roger.random@outlook.com>
2018-01-24 23:00:41 -0800

    Establish Roboclaw connection over serial

--- PiBotBrain/testconfig.py
+++ PiBotBrain/testconfig.py
@@ -1,24 +1,50 @@
 # App to configure Roboclaw and test PID values
 
-from flask import Flask, g, redirect, render_template, url_for
+from flask import Flask, flash, g, redirect, render_template, request, url_for
+import os
 from roboclaw import Roboclaw
 
 app = Flask(__name__)
 
+# Randomly generated key means session cookies will not be usable across 
+# instances. This flaw is acceptable for the test config app.
+app.secret_key = os.urandom(24)
+
+# Global Roboclaw - this is a terrible idea for web apps in general, but since
+# we are catering to a single user instance it is an ugly but sufficient hack.
+rc = None
+
+# Root menu
 @app.route('/')
 def root_menu():
-	rc = getattr(g, 'roboclaw', None)
 	if rc is None:
 		return redirect(url_for('connect_menu'))
-	return render_template("root_menu.html")
+	ret = rc.ReadVersion(0x80)
+	if ret[0]==1:
+		versionText = ret[1]
+		return render_template("root_menu.html", version=versionText)
+	return "Error encountered reading version from Roboclaw"
+
 
 # Connect menu let's the user specify serial port parameters for creating
 # a Roboclaw object
-
-@app.route('/connect')
+@app.route('/connect', methods=['GET', 'POST'])
 def connect_menu():
-	rc = getattr(g, 'roboclaw', None)
-	if rc is None:
-		return render_template("connect_menu.html")
+	if request.method == 'GET':
+		if rc is None:
+			return render_template("connect_menu.html")
+		else:
+			return redirect(url_for('root_menu'))
+	elif request.method == 'POST':
+		newrc = Roboclaw(request.form['port'],115200)
+		ret = newrc.Open()
+		if ret == 1:
+			global rc
+			rc = newrc
+			flash("Roboclaw API connected to specified serial port")
+			return redirect(url_for('root_menu'))
+		else:
+			flash("could not open specified port")
+			return redirect(url_for('connect_menu'))
 	else:
-		return redirect(url_for('root_menu'))
+		return "Unexpected method on connect"
