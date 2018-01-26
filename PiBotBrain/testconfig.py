60b9ec441655596846e7a74d493e5c337efea302
Roger Cheng <roger.random@outlook.com>
2018-01-25 23:43:51 -0800

    Initial config menu

--- PiBotBrain/testconfig.py
+++ PiBotBrain/testconfig.py
@@ -63,6 +63,24 @@ def checkRoboclawAddress():
 
 	return (rc, rcAddr)
 
+# Every read operation from the Roboclaw API returns a tuple: index zero
+# is 1 for success and 0 for failure. This helper looks for that zero and
+# raises an exception if one is seen. If an optional error message was 
+# provided, it is put into the flash before exception is raised.
+# In the normal case of success, if there was only one other element in
+# the resultTuple, that element is returned by itself (not a single
+# element tuple) If there are more tha one, the result is a tuple.
+def readResult(resultTuple, flashError=None):
+	if resultTuple[0] == 0:
+		if flashError is not None:
+			flash(flashError + " " + str(resultTuple))
+		raise ValueError
+
+	if len(resultTuple) == 2:
+		return resultTuple[1]
+	else:
+		return resultTuple[1:]
+
 # Root menu
 @app.route('/')
 def root_menu():
@@ -72,12 +90,13 @@ def root_menu():
 
 	displayMenu = False
 	if rcAddr is not None:
-		ret = rc.ReadVersion(rcAddr)
-		if ret[0]==1:
-			roboResponse = "Roboclaw at address {0} ({0:#x}) version: {1}".format(rcAddr, ret[1])
+		try:
+			verString = readResult(rc.ReadVersion(rcAddr))
+			roboResponse = "Roboclaw at address {0} ({0:#x}) version: {1}".format(rcAddr, verString)
 			displayMenu = True
-		else:
+		except ValueError as ve:
 			roboResponse = "No Roboclaw response from address {0} ({0:#x})".format(rcAddr)
+
 	else:
 		roboResponse = None
 
@@ -103,8 +122,7 @@ def connect_menu():
 
 		# Create the Roboclaw object against the specified serial port
 		newrc = Roboclaw(portName,baudrate,interCharTimeout,retries)
-		ret = newrc.Open()
-		if ret == 1:
+		if newrc.Open():
 			rc = newrc
 			flash("Roboclaw API connected to " + portName)
 			return redirect(url_for('root_menu', address="0x80"))
@@ -129,13 +147,33 @@ def config_menu():
 		elif request.method == 'POST':
 			return config_update(*rcAndAddr)
 		else:
+			flash("Unexpected request method on config")
 			return redirect(url_for('config_menu'))
 	except ValueError as ve:
 		return redirect(url_for('root_menu'))
 
 # Retrieve all the settings so we can show them in the rendered template.
 def config_read(rc, rcAddr):
-	return "Placeholder - Roboclaw configurations"
+	try:
+		rcVersion = readResult(rc.ReadVersion(rcAddr), "Version string")
+		VmainMin,VmainMax = readResult(rc.ReadMinMaxMainVoltages(rcAddr), "Main voltage min/max")
+		AmaxM1 = readResult(rc.ReadM1MaxCurrent(rcAddr), "Current max motor 1")
+		AmaxM2 = readResult(rc.ReadM2MaxCurrent(rcAddr), "Current max motor 2")
+		pwmMode = readResult(rc.ReadPWMMode(rcAddr), "PWM mode")
+		encModeM1,encModeM2 = readResult(rc.ReadEncoderModes(rcAddr),"Encoder mode")
+		s3, s4, s5 = readResult(rc.ReadPinFunctions(rcAddr),"Pin functions")
+
+		# Convert config bitfield to binary representation
+		rcConfig = "{0:#b}".format(readResult(rc.GetConfig(rcAddr), "Config"))
+
+		return render_template("config_menu.html", rcVersion=rcVersion, 
+			VmainMin=VmainMin, VmainMax=VmainMax, AmaxM1=AmaxM1, AmaxM2=AmaxM2, 
+			pwmMode=pwmMode, encModeM1=encModeM1, encModeM2=encModeM2,
+			s3=s3, s4=s4, s5=s5, rcConfig=rcConfig, rcAddr=rcAddr)
+
+	except ValueError as ve:
+		return redirect(url_for('root_menu'))
+	
 
 # Send values to Roboclaw
 def config_update(rc, rcAddr):
