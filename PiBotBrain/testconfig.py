4cca40f4de639e542ae3fd1dce77b0a03aa07d0e
Roger Cheng <roger.random@outlook.com>
2018-01-26 01:51:40 -0800

    Can now update config flags

--- PiBotBrain/testconfig.py
+++ PiBotBrain/testconfig.py
@@ -70,16 +70,27 @@ def checkRoboclawAddress():
 # In the normal case of success, if there was only one other element in
 # the resultTuple, that element is returned by itself (not a single
 # element tuple) If there are more tha one, the result is a tuple.
-def readResult(resultTuple, flashError=None):
+def readResult(resultTuple, flashMessage=None):
 	if resultTuple[0] == 0:
-		if flashError is not None:
-			flash(flashError + " " + str(resultTuple))
+		if flashMessage is not None:
+			flash("ERROR: {} {}".format(flashMessage, str(resultTuple)))
 		raise ValueError
 
 	if len(resultTuple) == 2:
 		return resultTuple[1]
 	else:
 		return resultTuple[1:]
+
+# Every write operation returns True if successful. This helper looks for a
+# False and raises an exception when it happens. Optional flash message is 
+# placed in flash prepended with ERROR or SUCCESS depending on condition.
+def writeResult(result, flashMessage=None):
+	if not result:
+		if flashMessage is not None:
+			flash("ERROR: {}".format(flashMessage))
+		raise ValueError
+	elif flashMessage is not None:
+		flash("SUCCESS: {}".format(flashMessage))
 
 # Root menu
 @app.route('/')
@@ -138,43 +149,65 @@ def connect_menu():
 @app.route('/config', methods=['GET', 'POST'])
 def config_menu():
 	try:
-		rcAndAddr = checkRoboclawAddress()
+		rc, rcAddr = checkRoboclawAddress()
 
-		# We have Roboclaw API object pointing to a Roboclaw that responds.
-		# Delegate further action to the appropriate helper method.
+		rcVersion = readResult(rc.ReadVersion(rcAddr), "Read version string")
+		VmainMin,VmainMax = readResult(rc.ReadMinMaxMainVoltages(rcAddr), "Read main voltage limits")
+		AmaxM1 = readResult(rc.ReadM1MaxCurrent(rcAddr), "Read motor 1 max current")
+		AmaxM2 = readResult(rc.ReadM2MaxCurrent(rcAddr), "Read motor 2 max current")
+		pwmMode = readResult(rc.ReadPWMMode(rcAddr), "Read PWM mode")
+		encModeM1,encModeM2 = readResult(rc.ReadEncoderModes(rcAddr),"Read encoder mode")
+		s3, s4, s5 = readResult(rc.ReadPinFunctions(rcAddr),"Read pin functions")
+
+		# Convert config bitfield to hex representation
+		rcConfig = "{0:#x}".format(readResult(rc.GetConfig(rcAddr), "Read config"))
+
 		if request.method == 'GET':
-			return config_read(*rcAndAddr)
+			return render_template("config_menu.html", rcVersion=rcVersion, 
+				VmainMin=VmainMin, VmainMax=VmainMax, AmaxM1=AmaxM1, AmaxM2=AmaxM2, 
+				pwmMode=pwmMode, encModeM1=encModeM1, encModeM2=encModeM2,
+				s3=s3, s4=s4, s5=s5, rcConfig=rcConfig, rcAddr=rcAddr)
 		elif request.method == 'POST':
-			return config_update(*rcAndAddr)
+			# TODO sanity validation of these values from the HTML form
+			fVmainMin = int(request.form['VmainMin'])
+			fVmainMax = int(request.form['VmainMax'])
+			fAmaxM1 = int(request.form['AmaxM1'])
+			fAmaxM2 = int(request.form['AmaxM2'])
+			fpwmMode = int(request.form['pwmMode'])
+			fencModeM1 = int(request.form['encModeM1'])
+			fencModeM2 = int(request.form['encModeM2'])
+			fs3 = int(request.form['s3'])
+			fs4 = int(request.form['s4'])
+			fs5 = int(request.form['s5'])
+			frcConfig = int(request.form['rcConfig'],16)
+
+			if fVmainMin != VmainMin or fVmainMax != VmainMax:
+				writeResult(rc.SetMainVoltages(rcAddr, fVmainMin, fVmainMax), "Update Main voltage limits")
+
+			if fAmaxM1 != AmaxM1:
+				writeResult(rc.SetM1MaxCurrent(rcAddr, fAmaxM1), "Update M1 max current")
+
+			if fAmaxM2 != AmaxM2:
+				writeResult(rc.SetM2MaxCurrent(rcAddr, fAmaxM2), "Update M2 max current")
+
+			if fpwmMode != pwmMode:
+				writeResult(rc.SetPWMMode(rcAddr, fpwmMode), "Update PWM mode")
+
+			if fencModeM1 != encModeM1:
+				writeResult(rc.SetM1EncoderMode(rcAddr, fencModeM1), "Update M1 encoder mode")
+
+			if fencModeM2 != encModeM2:
+				writeResult(rc.SetM2EncoderMode(rcAddr, fencModeM2), "Update M2 encoder mode")
+
+			if fs3 != s3 or fs4 != s4 or fs5 != s5:
+				writeResult(rc.SetPinFunctions(rcAddr, fs3, fs4, fs5), "Update S3/S4/S5 functions")
+
+			if frcConfig != int(rcConfig,16):
+				writeResult(rc.SetConfig(rcAddr, frcConfig), "Update config flags")
+
+			return redirect(url_for('config_menu',address=rcAddr))
 		else:
 			flash("Unexpected request method on config")
 			return redirect(url_for('config_menu'))
 	except ValueError as ve:
 		return redirect(url_for('root_menu'))
-
-# Retrieve all the settings so we can show them in the rendered template.
-def config_read(rc, rcAddr):
-	try:
-		rcVersion = readResult(rc.ReadVersion(rcAddr), "Version string")
-		VmainMin,VmainMax = readResult(rc.ReadMinMaxMainVoltages(rcAddr), "Main voltage min/max")
-		AmaxM1 = readResult(rc.ReadM1MaxCurrent(rcAddr), "Current max motor 1")
-		AmaxM2 = readResult(rc.ReadM2MaxCurrent(rcAddr), "Current max motor 2")
-		pwmMode = readResult(rc.ReadPWMMode(rcAddr), "PWM mode")
-		encModeM1,encModeM2 = readResult(rc.ReadEncoderModes(rcAddr),"Encoder mode")
-		s3, s4, s5 = readResult(rc.ReadPinFunctions(rcAddr),"Pin functions")
-
-		# Convert config bitfield to binary representation
-		rcConfig = "{0:#b}".format(readResult(rc.GetConfig(rcAddr), "Config"))
-
-		return render_template("config_menu.html", rcVersion=rcVersion, 
-			VmainMin=VmainMin, VmainMax=VmainMax, AmaxM1=AmaxM1, AmaxM2=AmaxM2, 
-			pwmMode=pwmMode, encModeM1=encModeM1, encModeM2=encModeM2,
-			s3=s3, s4=s4, s5=s5, rcConfig=rcConfig, rcAddr=rcAddr)
-
-	except ValueError as ve:
-		return redirect(url_for('root_menu'))
-	
-
-# Send values to Roboclaw
-def config_update(rc, rcAddr):
-	return redirect(url_for('config_menu'))
