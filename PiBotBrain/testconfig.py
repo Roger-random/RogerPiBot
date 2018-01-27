01c2f7484360936e7eb4947a0a6dc39111d4f439
Roger Cheng <roger.random@outlook.com>
2018-01-26 19:19:05 -0800

    NVM & velocity PID write (hack)

--- PiBotBrain/testconfig.py
+++ PiBotBrain/testconfig.py
@@ -186,29 +186,44 @@ def config_menu():
 			fs5 = int(request.form['s5'])
 			frcConfig = int(request.form['rcConfig'],16)
 
+			configUpdated = False
+
 			if fVmainMin != VmainMin or fVmainMax != VmainMax:
 				writeResult(rc.SetMainVoltages(rcAddr, fVmainMin, fVmainMax), "Update Main voltage limits")
+				configUpdated = True
 
 			if fAmaxM1 != AmaxM1:
 				writeResult(rc.SetM1MaxCurrent(rcAddr, fAmaxM1), "Update M1 max current")
+				configUpdated = True
 
 			if fAmaxM2 != AmaxM2:
 				writeResult(rc.SetM2MaxCurrent(rcAddr, fAmaxM2), "Update M2 max current")
+				configUpdated = True
 
 			if fpwmMode != pwmMode:
 				writeResult(rc.SetPWMMode(rcAddr, fpwmMode), "Update PWM mode")
+				configUpdated = True
 
 			if fencModeM1 != encModeM1:
 				writeResult(rc.SetM1EncoderMode(rcAddr, fencModeM1), "Update M1 encoder mode")
+				configUpdated = True
 
 			if fencModeM2 != encModeM2:
 				writeResult(rc.SetM2EncoderMode(rcAddr, fencModeM2), "Update M2 encoder mode")
+				configUpdated = True
 
 			if fs3 != s3 or fs4 != s4 or fs5 != s5:
 				writeResult(rc.SetPinFunctions(rcAddr, fs3, fs4, fs5), "Update S3/S4/S5 functions")
+				configUpdated = True
 
 			if frcConfig != int(rcConfig,16):
 				writeResult(rc.SetConfig(rcAddr, frcConfig), "Update config flags")
+				configUpdated = True
+
+			if configUpdated:
+				writeResult(rc.WriteNVM(rcAddr), "Write to non-volatile memory")
+				writeResult(rc.SetM1VelocityPID(0x80, 15000, 1000, 500, 3000), "Update M1 velocity PID")
+				writeResult(rc.SetM2VelocityPID(0x80, 15000, 1000, 500, 3000), "Update M2 velocity PID")
 
 			return redirect(url_for('config_menu',address=rcAddr))
 		else:
@@ -361,6 +376,7 @@ def to_position():
 	try:
 		rc,rcAddr = checkRoboclawAddress()
 
+		# TODO sanity validation of these values from the HTML form
 		# Pull values from form, update them in session dictionary.
 		session['m1accel'] = m1accel = int(request.form['m1accel'])		
 		session['m2accel'] = m2accel = int(request.form['m2accel'])
